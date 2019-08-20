02c642cf9af7e140e8f2f395764fa39dcea8efc5
Sasha Kolomiytsev <kolomiytsevs@googlemail.com>
2019-08-20 20:21:34 +0100

    Add tests to ConsultantSelectButton
    Check to make sure component calssName is changin depending on props
    being passed.

--- src/Components/ConsultantSelectButton.test.js
+++ src/Components/ConsultantSelectButton.test.js
@@ -1,27 +1,73 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
 import ConsultantSelectButton from './ConsultantSelectButton'
-import checkPropTypes from 'check-prop-types'
+import { checkProps } from '../../Utils'
+import Adapter from 'enzyme-adapter-react-16'
 
-const checkProps = (component, expectedProps) => {
-  const propsErr = checkPropTypes(
-    component.propTypes,
-    expectedProps,
-    'props',
-    component.name
-  )
-  return propsErr
-}
+Enzyme.configure({ adapter: new Adapter() })
+
+const findByClassName = (component, className) =>
+  component.find(`[className='${className}']`)
 
 describe('ConsultantSelectButton Component', () => {
   describe('Checking PropTypes', () => {
     it('Should NOT throw a prop type warning', () => {
       const expectedProps = {
         consultantType: 'GP',
+        selectedConsultantType: 'gp',
         handleConsultantSelect: () => {},
       }
       const propsError = checkProps(ConsultantSelectButton, expectedProps)
       expect(propsError).toBeUndefined()
     })
+    it('Should throw a prop type warning', () => {
+      const expectedProps = {
+        consultantType: [],
+        handleConsultantSelect: () => {},
+        selectedConsultantType: {},
+      }
+      const propsError = checkProps(ConsultantSelectButton, expectedProps)
+      expect(propsError).not.toBeNull()
+    })
+  })
+})
+
+describe('Renders', () => {
+  let wrapper
+  let mockFunc
+  beforeEach(() => {
+    mockFunc = jest.fn()
+    const props = {
+      consultantType: 'GP',
+      selectedConsultantType: 'therapist',
+      handleConsultantSelect: mockFunc,
+    }
+
+    wrapper = shallow(<ConsultantSelectButton {...props} />)
+  })
+
+  it('Should Render a button', () => {
+    const button = findByClassName(wrapper, 'button')
+    expect(button.length).toBe(1)
+  })
+
+  it('Should emit callback on click event', () => {
+    const button = findByClassName(wrapper, 'button')
+    button.simulate('click')
+    const callback = mockFunc.mock.calls.length
+    expect(callback).toBe(1)
+  })
+  it('should display consultant name', () => {
+    const text = wrapper.find('div')
+    expect(text.text()).toBe('GP')
+  })
+  it('should convert consultant type to lowercase and change className to selected when the value is the same as the selectedConsultantType', () => {
+    const props = {
+      consultantType: 'GP',
+      selectedConsultantType: 'gp',
+      handleConsultantSelect: mockFunc,
+    }
+    wrapper = shallow(<ConsultantSelectButton {...props} />)
+    expect(wrapper.props().className).toBe('selected')
   })
 })
